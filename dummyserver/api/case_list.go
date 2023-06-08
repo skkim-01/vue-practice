e7fcabeea3d728c149a45f7690cb90df5831fe1a
skkim-01 <lemoned222@gmail.com>
2023-06-08 16:18:54 +0900

    update: server: case list dummy

--- dummyserver/api/case_list.go
+++ dummyserver/api/case_list.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"github.com/skkim-01/vue-practice/dummyserver/results"
+
+	"github.com/tyler-smith/go-bip39"
 )
 
 func CaseList(w http.ResponseWriter, r *http.Request) {
@@ -16,17 +18,44 @@ func CaseList(w http.ResponseWriter, r *http.Request) {
 	SetCors(&w)
 	w.Header().Add("content-type", "application/json")
 
-	slResult := make([]map[string]interface{}, 3)
-	slResult[0] = _createDummyData()
-	slResult[1] = _createDummyData()
-	slResult[2] = _createDummyData()
+	// slResult := make([]map[string]interface{}, 3)
+	// slResult[0] = _createDummyData()
+	// slResult[1] = _createDummyData()
+	// slResult[2] = _createDummyData()
 
+	slResult := _createDummys(45)
 	result.Success("items", slResult)
 
 	fmt.Fprintf(w, "%v", result.Build())
 
 	tmElapsed = time.Since(tmStart)
-	fmt.Println("#DBG\t[GET] /v1/support-case\tresult:", result.Build(), "\ttook:", tmElapsed)
+	fmt.Println(fmt.Sprintf("#DBG\t[%v] %v%v\ttook: %v", r.Method, r.Host, r.URL, tmElapsed))
+	// fmt.Println(result.Build())
+}
+
+func _createDummys(count int) []map[string]interface{} {
+	retv := make([]map[string]interface{}, count)
+
+	for i := 0; i < count; i++ {
+		dummy := make(map[string]interface{}, 0)
+		dummy["account_id"] = fmt.Sprintf("account %v", (i+1)%5)
+		dummy["category_code"] = fmt.Sprintf("category %v", (i+1)%7)
+		dummy["display_id"] = fmt.Sprintf("display id %v", (i+1)%5)
+		dummy["language"] = "en"
+		dummy["service_code"] = fmt.Sprintf("svc code %v", (i+1)%13)
+		dummy["status"] = "OPENED" // TODO
+		entropy, _ := bip39.NewEntropy(256)
+		mnemonic, _ := bip39.NewMnemonic(entropy)
+		dummy["subject"] = mnemonic
+		dummy["severity_code"] = "CRITICAL" // TODO
+		dummy["onwer_email"] = "test@account.com"
+		dummy["use_yn"] = "y"
+		dummy["case_create_time"] = fmt.Sprintf("2023-06-07 %02d:%02d:%02d", (i+1)%3, (i+1)%58, (i+1)%58)
+		dummy["create_time"] = fmt.Sprintf("2023-06-07 %02d:%02d:%02d", (i+2)%7, (i+2)%23, (i+2)%29)
+		dummy["update_time"] = fmt.Sprintf("2023-06-07 %02d:%02d:%02d", (i+3)%11, (i+3)%37, (i+3)%18)
+		retv[i] = dummy
+	}
+	return retv
 }
 
 func _createDummyData() map[string]interface{} {
--- dummyserver/go.mod
+++ dummyserver/go.mod
@@ -2,6 +2,12 @@ module github.com/skkim-01/vue-practice/dummyserver
 
 go 1.20
 
-require github.com/skkim-01/json-mapper v0.0.0-20230504052053-1e47f12e38de
+require (
+	github.com/skkim-01/json-mapper v0.0.0-20230504052053-1e47f12e38de
+	github.com/tyler-smith/go-bip39 v1.1.0
+)
 
-require github.com/skkim-01/json-condition-parser v0.0.0-20210402012324-71c5e742e01b // indirect
+require (
+	github.com/skkim-01/json-condition-parser v0.0.0-20210402012324-71c5e742e01b // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+)
